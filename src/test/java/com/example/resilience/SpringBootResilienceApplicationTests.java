d147e1b62c5fc64908489adfcacb05aa4e777fce
HarshadRanganathan <rharshad93@gmail.com>
2021-10-07 16:39:52 +0100

    initial commit

---
+++ src/test/java/com/example/resilience/SpringBootResilienceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.resilience;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootResilienceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
